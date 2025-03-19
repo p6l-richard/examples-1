5aaff1d33e0891d5aa5eac1839da4a77b5e80ca2
Richard Poelderl <richard.poelderl@gmail.com>
2025-03-19 15:58:48 +0100

    chore: update public files with correct URLs

--- form-static/public/body.js
+++
@@ -1,7 +0,0 @@
-export default function injectScripts() {
-    const publicUrl = 'https://p6l-richard.github.io/examples-1/form-static/public';
-    document.body.insertAdjacentHTML('beforeend', `
-      <script src="${publicUrl}/Form.f4926498.js" type="module"></script>
-      <script>(self.__FLIGHT_DATA ||= []).push("...")</script>
-    `);
-  }
--- form-static/public/head.js
+++
@@ -1,7 +0,0 @@
-export default function injectHead() {
-    const head = document.head;
-    const publicUrl = 'https://p6l-richard.github.io/examples-1/form-static/public';
-    head.insertAdjacentHTML('beforeend', `
-      <link rel="stylesheet" href="${publicUrl}/Form.4a3e2ace.css">
-    `);
-  }
