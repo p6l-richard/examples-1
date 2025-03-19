d97d9b8424cda0159dfed487640e50a46f1693a1
Richard Poelderl <richard.poelderl@gmail.com>
2025-03-19 16:00:27 +0100

    chore: rebuild with cleaned public directory

--- form-static/scripts/split-output.js
+++ form-static/scripts/split-output.js
@@ -2,39 +2,30 @@ const fs = require('fs');
 const path = require('path');
 
 // Read the Form.html file
-const htmlPath = path.join(__dirname, '../public/Form.html');
+const htmlPath = path.join(__dirname, '../dist/Form.html');
 const html = fs.readFileSync(htmlPath, 'utf8');
 
 // Extract parts using regex
-const headContent = html.match(/<head[^>]*>([\s\S]*?)<\/head>/i)?.[1] || '';
+const headLinks = html.match(/<link[^>]*>/g) || [];
 const bodyScripts = html.match(/<script[\s\S]*?<\/script>/g) || [];
 const divContent = html.match(/<div class="form-widget"[\s\S]*?<\/div>/i)?.[0] || '';
 
-// Write head.js
+// Write head.html
 fs.writeFileSync(
-  path.join(__dirname, '../public/head.js'),
-  `export default function injectHead() {
-    const head = document.head;
-    const publicUrl = 'https://p6l-richard.github.io/examples-1/form-static/public';
-    head.insertAdjacentHTML('beforeend', \`
-      <link rel="stylesheet" href="\${publicUrl}/Form.4a3e2ace.css">
-    \`);
-  }`
+  path.join(__dirname, '../dist/head.html'),
+  headLinks.join('\n')
 );
 
-// Write body.js
+// Write body.html
 fs.writeFileSync(
-  path.join(__dirname, '../public/body.js'),
-  `export default function injectScripts() {
-    const publicUrl = 'https://p6l-richard.github.io/examples-1/form-static/public';
-    document.body.insertAdjacentHTML('beforeend', \`
-      <script src="\${publicUrl}/Form.f4926498.js" type="module"></script>
-      <script>(self.__FLIGHT_DATA ||= []).push("...")</script>
-    \`);
-  }`
+  path.join(__dirname, '../dist/body.html'),
+  bodyScripts.join('\n')
 );
 
-// Write content.html
-fs.writeFileSync(path.join(__dirname, '../public/content.html'), divContent);
+// Overwrite Form.html (just the widget content)
+fs.writeFileSync(
+  path.join(__dirname, '../dist/Form.html'), 
+  divContent
+);
 
-console.log('Successfully split output into head.js, body.js, and content.html'); 
+console.log('Successfully split output into head.html, body.html, and Form.html'); 
